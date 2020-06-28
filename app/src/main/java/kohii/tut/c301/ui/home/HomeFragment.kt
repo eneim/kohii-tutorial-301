c67adf4f68a92ab7c91d74588bd4b3e422e7529f
eneim <1776230+eneim@users.noreply.github.com>
2020-06-28 13:12:08 +0900

    feat: update dependencies

--- app/src/main/java/kohii/tut/c301/ui/home/HomeFragment.kt
+++ app/src/main/java/kohii/tut/c301/ui/home/HomeFragment.kt
@@ -7,7 +7,7 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
+import androidx.lifecycle.ViewModelProvider
 import kohii.tut.c301.R
 
 class HomeFragment : Fragment() {
@@ -19,9 +19,7 @@ class HomeFragment : Fragment() {
     container: ViewGroup?,
     savedInstanceState: Bundle?
   ): View? {
-    homeViewModel =
-      ViewModelProviders.of(this)
-          .get(HomeViewModel::class.java)
+    homeViewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
     val root = inflater.inflate(R.layout.fragment_home, container, false)
     val textView: TextView = root.findViewById(R.id.text_home)
     homeViewModel.text.observe(viewLifecycleOwner, Observer {

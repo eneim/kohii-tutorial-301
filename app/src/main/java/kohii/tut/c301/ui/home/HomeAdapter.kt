afbbd718b2906cf84a5852ecf08c3f43b1ea22d3
eneim <1776230+eneim@users.noreply.github.com>
2020-06-28 18:54:36 +0900

    feat: add user interaction

--- app/src/main/java/kohii/tut/c301/ui/home/HomeAdapter.kt
+++ app/src/main/java/kohii/tut/c301/ui/home/HomeAdapter.kt
@@ -5,6 +5,8 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView.Adapter
 import kohii.tut.c301.R
 import kohii.v1.core.Common
+import kohii.v1.core.Playback
+import kohii.v1.core.Playback.Controller
 import kohii.v1.exoplayer.Kohii
 
 class HomeAdapter(
@@ -24,6 +26,24 @@ class HomeAdapter(
       tag = "${videoItem.videoUrl}+${position}"
       preload = true
       repeatMode = Common.REPEAT_MODE_ONE
+      controller = object : Controller {
+        override fun kohiiCanStart(): Boolean = true
+        override fun kohiiCanPause(): Boolean = true
+        override fun setupRenderer(playback: Playback, renderer: Any?) {
+          holder.playerContainer.setOnClickListener {
+            val playable = playback.playable ?: return@setOnClickListener
+            if (playable.isPlaying()) {
+              playback.manager.pause(playable)
+            } else {
+              playback.manager.play(playable)
+            }
+          }
+        }
+
+        override fun teardownRenderer(playback: Playback, renderer: Any?) {
+          playback.container.setOnClickListener(null)
+        }
+      }
     }.bind(holder.playerView)
   }
 }
--- app/src/main/java/kohii/tut/c301/ui/home/PlayerViewHolder.kt
+++ app/src/main/java/kohii/tut/c301/ui/home/PlayerViewHolder.kt
@@ -7,5 +7,6 @@ import kohii.tut.c301.R
 
 class PlayerViewHolder(itemView: View) : ViewHolder(itemView) {
 
+  val playerContainer: ViewGroup = itemView.findViewById(R.id.playerContainer)
   val playerView: ViewGroup = itemView.findViewById(R.id.playerView)
 }
